74252dce49afa701d17348df6f58a5aee4cf5391
KStaeubleInf22 <111045576+KStaeubleInf22@users.noreply.github.com>
2023-09-22 16:19:17 +0200

    Add files via upload

---
+++ random number gen/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace random_number_gen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int val = r.Next(1, 100);
+            int guess = 0;
+            bool correct = false;
+            string Secretword = "Show";
+            string Easteregg = "Easteregg";
+
+
+
+            int attempts = 0;
+
+            Console.WriteLine("I'm thinking of a number between 1 and 100. Can you guess it?");
+
+            Console.WriteLine("You only have 7 tries.");
+            while (!correct && attempts < 7)
+            {
+                Console.Write("Guess: ");
+                string input = Console.ReadLine();
+
+                if (input == Secretword)
+                {
+                    Console.WriteLine(val);
+                }
+                if (input == Easteregg)
+                {
+                    Console.WriteLine("Hello and welcome to the Los Pollos Hermanos family");
+                    Console.WriteLine("My name is Gustavo but you can call me Gus");
+                }
+
+                if (!int.TryParse(input, out guess))
+                {
+
+                    continue;
+                }
+                if (guess < val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is higher than that number.");
+                    attempts++;
+                    Console.WriteLine("You wasted " + attempts + " attempts of 7.");
+                    
+                }
+                else if (guess > val)
+                {
+                    Console.WriteLine("No, the number I'm thinking is lower than that number.");
+                    attempts++;
+                    Console.WriteLine("You wasted " + attempts + " attempts of 7.");
+                }
+                else
+                {
+                    correct = true;
+                    Console.WriteLine("You guessed right!");
+
+                   
+                    if (attempts == 7)
+                    {
+                        Console.WriteLine("You failed!");
+                        break;
+                    }
+                    
+                    
+
+                }
+            }
+        }
+    }
+}
+                
+   
+
+
+
+    
